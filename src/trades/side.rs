c9b0f5abda08c42a2d8fb570fa77a4bdf8d95d04
Valera6 <v79166789533@gmail.com>
2024-01-20 01:27:54 +0000

    .

--- src/trades/side.rs
+++ src/trades/side.rs
@@ -1,7 +1,20 @@
-#[derive(Debug, Clone, Copy)]
+use std::str::FromStr;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Side {
 	Buy,
 	Sell,
+}
+impl FromStr for Side {
+	type Err = String;
+
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		match s.to_uppercase().as_str() {
+			"BUY" => Ok(Side::Buy),
+			"SELL" => Ok(Side::Sell),
+			_ => Err(format!("Invalid side: {}", s)),
+		}
+	}
 }
 impl Side {
 	pub fn to_str(&self) -> &'static str {
@@ -14,5 +27,23 @@ impl Side {
 impl std::fmt::Display for Side {
 	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
 		self.to_str().fmt(f)
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_side_from_str() {
+		assert_eq!(Side::from_str("BUY").unwrap(), Side::Buy);
+		assert_eq!(Side::from_str("Sell").unwrap(), Side::Sell);
+		assert!(Side::from_str("foo").is_err());
+	}
+
+	#[test]
+	fn test_side_to_str() {
+		assert_eq!(Side::Buy.to_str(), "BUY");
+		assert_eq!(Side::Sell.to_str(), "SELL");
 	}
 }
