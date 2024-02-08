d664c9dcaff5b18698d453c168c1f0197c420f83
Valera6 <v79166789533@gmail.com>
2024-02-08 21:10:02 +0000

    feat: progress bar

--- v_utils/src/io/mod.rs
+++ v_utils/src/io/mod.rs
@@ -1,5 +1,7 @@
 mod cli;
 mod expanded_path;
+mod progress_bar;
 
 pub use cli::*;
 pub use expanded_path::*;
+pub use progress_bar::*;
---
+++ v_utils/src/io/progress_bar.rs
@@ -0,0 +1,35 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
+#[derive(Clone, Debug)]
+pub struct ProgressBar {
+	bar_width: f64,
+	timestamp_ms: u128,
+	total: f64,
+}
+impl ProgressBar {
+	pub fn new(total: usize) -> Self {
+		let bar_width: f64 = 133.0;
+		let timestamp_ms = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_millis();
+		let total = total as f64;
+		ProgressBar {
+			bar_width,
+			timestamp_ms,
+			total,
+		}
+	}
+
+	pub fn progress(&mut self, i: usize) {
+		const CLEAR: &str = "\x1B[2J\x1B[1;1H";
+		let scalar: f64 = self.bar_width / self.total;
+		let display_i = (i as f64 * scalar) as usize;
+		let display_total = (self.total * scalar) as usize;
+
+		println!("{}", CLEAR);
+		println!("[{}{}]", "*".repeat(display_i), " ".repeat(display_total - display_i));
+
+		let since_timestamp_ms = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis() - self.timestamp_ms;
+		let progress_left_scalar = (self.total - i as f64) / i as f64;
+		let left_s = (since_timestamp_ms as f64 * progress_left_scalar / 1000.0) as usize;
+		println!("Time left: ≈ {}s", left_s);
+	}
+}
