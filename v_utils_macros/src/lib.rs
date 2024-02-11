78995fcd43b2a2c7dbab861d67e72d1a11d7c28a
Valera6 <v79166789533@gmail.com>
2024-02-11 13:10:55 +0000

    feat: impl<S: AsRef<std::ffi::OsStr>> TryFrom<Vec<S>>

--- v_utils_macros/src/lib.rs
+++ v_utils_macros/src/lib.rs
@@ -48,46 +48,79 @@ pub fn graphemics(input: TokenStream) -> TokenStream {
 	TokenStream::from(expanded)
 }
 
-///BUG: will not work if any of the child structs share the same acronym and same fields.
+/// Put on a struct with optional fields, each of which implements FromStr
+///BUG: may write to the wrong field, if any of the child structs share the same acronym and same fields. In reality, shouldn't happen.
 #[proc_macro_derive(FromVecString)]
 pub fn derive(input: TokenStream) -> TokenStream {
 	let ast = parse_macro_input!(input as syn::DeriveInput);
 	let name = &ast.ident;
+	let fields = if let syn::Data::Struct(syn::DataStruct {
+		fields: syn::Fields::Named(syn::FieldsNamed { ref named, .. }),
+		..
+	}) = ast.data
+	{
+		named
+	} else {
+		unimplemented!()
+	};
+
+	let init_nones = fields.iter().map(|f| {
+		let ident = &f.ident;
+		let ty = &f.ty;
+		quote! {
+			let mut #ident: #ty = None;
+		}
+	});
+
+	let write_fields = fields.iter().map(|f| {
+		let ident = &f.ident;
+		quote! {
+			#ident,
+		}
+	});
+
+	let conversions = fields.iter().map(|field| {
+		let field_name = &field.ident;
+		let field_type = match &field.ty {
+			syn::Type::Path(type_path) if type_path.path.segments.last().unwrap().ident == "Option" => {
+				let generic_arg = match type_path.path.segments.last().unwrap().arguments {
+					syn::PathArguments::AngleBracketed(ref args) => &args.args[0],
+					_ => panic!("Expected generic argument for Option"),
+				};
+				quote! { #generic_arg }
+			}
+			_ => panic!("All fields must be of type Option<T>"),
+		};
+
+		quote! {
+			if #field_name.is_none() {
+				if let std::result::Result::Ok(value) = s.as_ref().to_str().unwrap_or("").parse::<#field_type>() {
+					#field_name = core::option::Option::Some(value);
+					continue;
+				}
+			}
+		}
+	});
 
 	let expanded = quote! {
-		//impl From<Vec<String>> for #name {
-		//	fn from(v: Vec<String>) -> Self {
-		//		#name {
-		//			#(
-		//				#name::#name_variant: v[#index].parse().unwrap(),
-		//			)*
-		//		}
-		//	}
-		//}
+		impl<S: AsRef<std::ffi::OsStr>> TryFrom<Vec<S>> for #name {
+			type Error = &'static str;
+
+			fn try_from(strings: Vec<S>) -> core::result::Result<Self, Self::Error> {
+				#(#init_nones)*
+
+				for s in strings {
+					#(#conversions)*
+
+					return std::result::Result::Err("Could not parse string");
+				}
+
+				std::result::Result::Ok(#name {
+					#(#write_fields)*
+				})
+			}
+		}
 	};
-	eprintln!("Hello, {}!", stringify!(#name));
 
 	expanded.into()
 }
-
-//? derive what? I need it to be able to deserialize from Vec<String>
-//#[proc_macro_derive()]
-//pub fn derive(input: TokenStream) -> TokenStream {
-//	let ast = parse_macro_input!(input as syn::DeriveInput);
-//	let name = &ast.ident;
-//
-//	let expanded = quote! {
-//		impl From<&str> for #name {
-//			fn from(s: &str) -> Self {
-//				let mut split = s.split_whitespace();
-//				#name {
-//					#(
-//						#name::#name_variant: split.next().unwrap().parse().unwrap(),
-//					)*
-//				}
-//			}
-//		}
-//	};
-//
-//	expanded.into()
-//}
--- v_utils_macros/tests/02-derive.rs
+++ v_utils_macros/tests/02-derive.rs
@@ -1,9 +1,13 @@
 use v_utils_macros::FromVecString;
 
-#[derive(FromVecString)]
-pub struct Test {
-	a: i32,
-	b: i32,
+#[derive(FromVecString, Debug, Clone)]
+pub struct Protocols {
+	pub trailing_stop: Option<f64>,
+	pub take_profit_stop_loss: Option<f64>,
+	pub leading_crosses: Option<f64>,
 }
 
-fn main() {}
+fn main() {
+	let _ = Protocols::try_from(vec!["0.1".to_owned(), "0.2".to_owned(), "0.3".to_owned()]);
+	let _ = Protocols::try_from(vec!["0.1", "0.2", "0.3"]);
+}
