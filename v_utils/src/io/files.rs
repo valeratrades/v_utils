dc9e940b82180dee3e64c9ec2b499cae0ec92a4f
Valera6 <v79166789533@gmail.com>
2024-04-05 12:02:17 +0000

    .

--- v_utils/src/io/files.rs
+++ v_utils/src/io/files.rs
@@ -40,12 +40,29 @@ pub fn open_with_mode(path: &PathBuf, mode: OpenMode) -> Result<()> {
 
 /// Wrapper around `open_with_mode` that syncs with git. If `open_mode` provided, it will open the file in-between.
 pub fn sync_file_with_git(path: &PathBuf, open_mode: Option<OpenMode>) -> Result<()> {
-	let p = path.display();
+	let metadata = match std::fs::metadata(path) {
+		Ok(metadata) => metadata,
+		Err(e) => match open_mode {
+			Some(OpenMode::Force) => {
+				std::fs::File::create(path).with_context(|| format!("Failed to force-create file at '{}'.\n{e}", path.display()))?;
+				std::fs::metadata(path).unwrap()
+			}
+			_ => anyhow::bail!(
+				"Failed to read metadata of file/directory at '{}', which means we do not have sufficient permissions or it does not exist",
+				path.display()
+			),
+		},
+	};
+	let sp = match metadata.is_dir() {
+		true => path.display(),
+		false => path.parent().unwrap().display(),
+	};
+
 	Command::new("sh")
 		.arg("-c")
-		.arg(format!("git -C \"{p}\" pull"))
+		.arg(format!("git -C \"{sp}\" pull"))
 		.status()
-		.with_context(|| format!("Failed to pull from Git repository at '{}'. Ensure a repository exists at this path or any of its parent directories and no merge conflicts are present.", p))?;
+		.with_context(|| format!("Failed to pull from Git repository at '{}'. Ensure a repository exists at this path or any of its parent directories and no merge conflicts are present.", sp))?;
 
 	if let Some(open_mode) = open_mode {
 		open_with_mode(path, open_mode).with_context(|| {
@@ -58,10 +75,10 @@ pub fn sync_file_with_git(path: &PathBuf, open_mode: Option<OpenMode>) -> Result
 
 	Command::new("sh")
 		.arg("-c")
-		.arg(format!("git -C \"{p}\" add -A && git -C \"{p}\" commit -m \".\" && git -C \"{p}\" push"))
+		.arg(format!("git -C \"{sp}\" add -A && git -C \"{sp}\" commit -m \".\" && git -C \"{sp}\" push"))
 		.status()
 		.with_context(|| {
-			format!("Failed to commit or push to Git repository at '{}'. Ensure you have the necessary permissions and the repository is correctly configured.", p)
+			format!("Failed to commit or push to Git repository at '{}'. Ensure you have the necessary permissions and the repository is correctly configured.", sp)
 		})?;
 
 	Ok(())
