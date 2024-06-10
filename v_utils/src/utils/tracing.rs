cf56ac26357616bc3d275221e38500087ad4560b
Valera6 <v79166789533@gmail.com>
2024-06-10 18:42:02 +0000

    _

--- v_utils/src/utils/tracing.rs
+++ v_utils/src/utils/tracing.rs
@@ -10,9 +10,19 @@ use tracing_subscriber::{
 
 ///# Panics
 pub fn init_subscriber() {
+	{
+		let mut rust_log = std::env::var("RUST_LOG").unwrap_or_else(|_| "".to_owned());
+		if !rust_log.is_empty() {
+			rust_log.push(',');
+		}
+		rust_log.push_str("tokio=trace,runtime=trace");
+		unsafe { std::env::set_var("RUST_LOG", rust_log) };
+	}
+
 	let env_filter = EnvFilter::try_from_default_env().unwrap_or(EnvFilter::new("info"));
 
 	// Fucking rust. And No, you can't make this shit with any less duplication, without sacrificing your soul.
+	// Only difference is `std::io::{stdout,sink}`
 	if std::env::var("TEST_LOG").is_ok() {
 		let formatting_layer = BunyanFormattingLayer::new("discretionary_engine".into(), std::io::stdout);
 		let subscriber = Registry::default().with(env_filter).with(JsonStorageLayer).with(formatting_layer);
@@ -21,7 +31,7 @@ pub fn init_subscriber() {
 		let formatting_layer = BunyanFormattingLayer::new("discretionary_engine".into(), std::io::sink);
 		let subscriber = Registry::default().with(env_filter).with(JsonStorageLayer).with(formatting_layer);
 		set_global_default(subscriber).expect("Failed to set subscriber");
-	};
+	}
 
 	//let formatting_layer = fmt::layer().json().pretty().with_writer(std::io::stdout);
 	trace_the_init();
