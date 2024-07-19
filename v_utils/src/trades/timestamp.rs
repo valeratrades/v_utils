3887b6b3615ac760c8986f4424fa95eb7c432d00
valeratrades <v79166789533@gmail.com>
2024-07-19 16:18:47 +0000

    fix: retarded old Timestamp implementation

--- v_utils/src/trades/timestamp.rs
+++ v_utils/src/trades/timestamp.rs
@@ -1,151 +1,25 @@
-use chrono::{DateTime, Utc};
-use std::time::{SystemTime, UNIX_EPOCH};
+use anyhow::{anyhow, Result};
+use chrono::{DateTime, TimeZone, Utc};
 
-#[derive(Clone)]
-pub struct Timestamp {
-	pub ns: i64,
-	pub s: i64,
-	pub ms: i64,
-	pub us: i64,
-	pub dt: DateTime<Utc>,
-	pub iso: String,
-}
-
-/// Core
-///
-impl Timestamp {
-	pub fn get_ms(&self) -> &i64 {
-		&self.ms
+/// Doesn't support negative timestamps
+fn guess_timestamp_unsafe(timestamp: String) -> Result<DateTime<Utc>> {
+	// Try parsing as ISO 8601 format
+	if let Ok(dt) = timestamp.parse::<DateTime<Utc>>() {
+		return Ok(dt);
 	}
 
-	fn from_ns(ns: i64) -> Self {
-		let s = ns / 1_000_000_000;
-		let ms = ns / 1_000_000;
-		let us = ns / 1_000;
-		let dt = DateTime::from_timestamp_micros(us).unwrap();
-		let iso = dt.to_rfc3339();
-		Timestamp { ns, s, ms, us, dt, iso }
-	}
-}
-impl From<i64> for Timestamp {
-	fn from(timestamp: i64) -> Self {
-		let len = timestamp.to_string().len();
-		let ns = match len {
-			10 => timestamp * 1_000_000_000,
-			13 => timestamp * 1_000_000,
-			16 => timestamp * 1_000,
-			19 => timestamp,
-			_ => panic!("Provided timestamp type isn't supported: {}", timestamp),
+	// Try guessing the denominator
+	if let Ok(num) = timestamp.parse::<u64>() {
+		let len = timestamp.len();
+		let nanos = match len {
+			10 => num * 1_000_000_000,
+			13 => num * 1_000_000,
+			16 => num * 1_000,
+			19 => num,
+			_ => return Err(anyhow!("Invalid timestamp length for guessing: {len}\nTimestamp: {timestamp}")),
 		};
-		Timestamp::from_ns(ns)
-	}
-}
-impl From<i32> for Timestamp {
-	fn from(timestamp: i32) -> Self {
-		let len = timestamp.to_string().len();
-		let ns = match len {
-			10 => timestamp as i64 * 1_000_000_000,
-			_ => panic!("Provided timestamp type isn't supported: {}", timestamp),
-		};
-		Timestamp::from_ns(ns)
-	}
-}
-impl From<u64> for Timestamp {
-	fn from(timestamp: u64) -> Self {
-		let len = timestamp.to_string().len();
-		let ns = match len {
-			10 => timestamp * 1_000_000_000,
-			13 => timestamp * 1_000_000,
-			16 => timestamp * 1_000,
-			19 => timestamp,
-			_ => panic!("Provided timestamp type isn't supported: {}", timestamp),
-		};
-		Timestamp::from_ns(ns as i64)
-	}
-}
-impl From<u32> for Timestamp {
-	fn from(timestamp: u32) -> Self {
-		let len = timestamp.to_string().len();
-		let ns = match len {
-			10 => timestamp as i64 * 1_000_000_000,
-			_ => panic!("Provided timestamp type isn't supported: {}", timestamp),
-		};
-		Timestamp::from_ns(ns)
-	}
-}
-impl From<&str> for Timestamp {
-	fn from(timestamp: &str) -> Self {
-		let dt = timestamp
-			.parse::<DateTime<Utc>>()
-			.unwrap_or_else(|_| panic!("Invalid ISO format: {}", timestamp));
-		let ns = dt.timestamp_nanos_opt().unwrap();
-		Timestamp::from_ns(ns)
-	}
-}
-impl From<DateTime<Utc>> for Timestamp {
-	fn from(timestamp: DateTime<Utc>) -> Self {
-		let ns = timestamp.timestamp_nanos_opt().unwrap();
-		Timestamp::from_ns(ns)
-	}
-}
-impl From<SystemTime> for Timestamp {
-	fn from(timestamp: SystemTime) -> Self {
-		let duration_since_epoch = timestamp.duration_since(UNIX_EPOCH).unwrap();
-		let ns = duration_since_epoch.as_nanos() as i64;
-		Timestamp::from_ns(ns)
-	}
-}
-
-/// additional direct functions
-///
-impl Timestamp {
-	/// Creates timestamp object using current time
-	pub fn now() -> Self {
-		let now_ms = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_millis() as i64;
-		Self::from_ns(now_ms * 1_000_000)
+		return Ok(Utc.timestamp_nanos(nanos as i64));
 	}
 
-	pub fn new() -> Self {
-		Self::now()
-	}
-
-	/// mutably subtracts defined number of seconds from all fields of the object
-	pub fn subtract<T: Into<i64>>(&self, s: T) -> Self {
-		let ns_to_subtract = s.into() * 1_000_000_000;
-		Self::from_ns(self.ns - ns_to_subtract)
-	}
-
-	pub fn add<T: Into<i64>>(&self, s: T) -> Self {
-		let ns_to_add = s.into() * 1_000_000_000;
-		Self::from_ns(self.ns + ns_to_add)
-	}
-}
-
-/// good practices:
-///
-use std::fmt;
-impl fmt::Debug for Timestamp {
-	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-		write!(f, "{:?}", self.iso)
-	}
-}
-
-impl Default for Timestamp {
-	fn default() -> Self {
-		Timestamp::now()
-	}
-}
-
-#[cfg(test)]
-mod types_timestamp {
-	use super::*;
-
-	#[test]
-	fn test_new_and_substract() {
-		let t = Timestamp::now();
-		use std::time::SystemTime;
-		let now_s = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs() as i64;
-		assert_eq!(t.s, now_s);
-		assert_eq!(t.subtract(100 * 5 * 60).s, now_s - 100 * 5 * 60);
-	}
+	Err(anyhow!("Couldn't parse timestamp: {}", timestamp))
 }
