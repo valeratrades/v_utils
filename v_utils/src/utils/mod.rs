49c49f37f18ca41a27c4a6f1b589a021ef86bfcf
valeratrades <valeratrades@gmail.com>
2025-01-15 00:09:43 +0000

    _

--- v_utils/src/utils/mod.rs
+++ v_utils/src/utils/mod.rs
@@ -12,31 +12,10 @@ pub mod tracing;
 #[cfg(feature = "tracing")]
 pub use tracing::*;
 
-//use std::collections::HashMap;
-//use thiserror::Error;
-//
-//#[derive(Error, Debug)]
-//#[error("Columns in provided data are not aligned")]
-//pub struct NotAlignedError;
-//
-////TODO!: make the now name String be a str-vec
-///// Fuck polars, this is my way to represent alignment property
-//#[derive(Clone, Debug, Default)]
-////TODO!: write the operations, right now there is no protection from updates that would break alignment property
-//pub struct Df<T: Clone + std::fmt::Debug, I: Clone + std::fmt::Debug>(pub HashMap<String, Vec<T>>);
-//
-//impl<T: Clone + std::fmt::Debug> Df<T> {
-//	pub fn new(map: HashMap<String, Vec<T>>) -> Result<Self, NotAlignedError> {
-//		let first_len = match map.iter().next() {
-//			Some((_, v)) => v.len(),
-//			None => return Ok(Self(HashMap::new())),
-//		};
-//		for col in map.iter() {
-//			if col.1.len() != first_len {
-//				return Err(NotAlignedError);
-//			}
-//		}
-//
-//		Ok(Self(map))
-//	}
-//}
+#[macro_export]
+macro_rules! clientside {
+	() => {
+		color_eyre::install().unwrap();
+		v_utils::utils::init_subscriber(v_utils::utils::LogDestination::xdg(env!("CARGO_PKG_NAME")));
+	};
+}
