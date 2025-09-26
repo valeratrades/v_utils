ae892da60eba1e6a6d38ac3e0fd4967ce220321c
Valera <valeratrades@gmail.com>
2025-09-26 13:52:44 +0000

    _

--- v_utils_macros/tests/main.rs
+++ v_utils_macros/tests/main.rs
@@ -8,6 +8,8 @@ fn tests() {
 	t.pass("tests/make_df.rs");
 	t.pass("tests/wrap_new.rs");
 	t.pass("tests/scream_it.rs");
-	#[cfg(feature = "cli")]
-	t.pass("tests/derive_settings.rs");
+	//TODO: reinstate or deprecate {{{
+	//#[cfg(feature = "cli")]
+	//t.pass("tests/derive_settings.rs");
+	//,}}}
 }
