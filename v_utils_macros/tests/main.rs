d55e649925093330600f0d795462c0113643ef53
Valera <valeratrades@gmail.com>
2025-10-17 01:03:50 +0000

    chore: _

--- v_utils_macros/tests/main.rs
+++ v_utils_macros/tests/main.rs
@@ -8,8 +8,6 @@ fn tests() {
 	t.pass("tests/make_df.rs");
 	t.pass("tests/wrap_new.rs");
 	t.pass("tests/scream_it.rs");
-	//TODO: reinstate or deprecate {{{
-	//#[cfg(feature = "cli")]
-	//t.pass("tests/derive_settings.rs");
-	//,}}}
+	#[cfg(feature = "cli")]
+	t.pass("tests/derive_settings.rs");
 }
